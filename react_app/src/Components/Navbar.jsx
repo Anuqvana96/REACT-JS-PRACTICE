f2bf59986efe49b2356b6d642cdfa503fa517b9b
Anuqvana96 <anuqvana9606@gmail.com>
2023-05-22 11:10:46 +0530

    practice

---
+++ react_app/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+     <nav>
+        <a href="/">Greet labs</a>
+
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/Services">Services</a></li>
+            <li><a href="/About us">About us</a></li>
+            <li><a href="/Contact">Contact</a></li>
+        </ul>
+     </nav>
+    </>
+  )
+}
+
+export default Navbar
